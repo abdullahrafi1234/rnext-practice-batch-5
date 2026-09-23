61d2f8e2dcafa0a0fcb5e0f36f1d6d173bc7a552
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2026-09-23 11:56:46 +0600

    added generic button in click event

--- module-2/src/App.jsx
+++ module-2/src/App.jsx
@@ -1,19 +1,50 @@
+// export default function App() {
+//   function handleClick() {
+//     console.log("Button clicked");
+//   }
+
+//   return (
+//     <button className="bg-amber-200" onClick={handleClick}>
+//       Click Here
+//     </button>
+
+//     // <button
+//     //   className="bg-amber-200"
+//     //   onClick={function handleClick() {
+//     //     console.log("Button clicked");
+//     //   }}
+//     // >
+//     //   Click Here
+//     // </button>
+
+//     // <button
+//     //   onClick={() => {
+//     //     alert("You clicked on arrow function");
+//     //   }}
+//     // >
+//     //   I dont do anything
+//     // </button>
+//   );
+// }
+
 export default function App() {
-  function handleClick() {
-    console.log("Button clicked");
-  }
-
   return (
-    // <button className="bg-amber-200" onClick={handleClick}>
-    //   Click Here
-    // </button>
-
-    <button
-      onClick={() => {
-        alert("You clicked");
-      }}
-    >
-      I dont do anything
-    </button>
+    <Toolbar
+      onPlayMovie={() => alert("Playing!")}
+      onUploadImage={() => alert("Uploading!")}
+    />
   );
+}
+
+function Toolbar({ onPlayMovie, onUploadImage }) {
+  return (
+    <div>
+      <Button onSmash={onPlayMovie}>Play Movie</Button>
+      <Button onSmash={onUploadImage}>Upload Image</Button>
+    </div>
+  );
+}
+
+function Button({ onSmash, children }) {
+  return <button onClick={onSmash}>{children}</button>;
 }
